015b3d86df8a6c4d957b426c32333c3918701943
Hyeongjun Ham <hjham0856@gmail.com>
2025-10-30 19:54:01 +0900

    feat: gemini API 사용 설정 - requirements.txt에 google의 genai 의존성 추가 - lambda_handler 함수가 들어온 요청에 대해서 gemini의 응답을 반환하도록 수정

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,8 +1,16 @@
 import json
+import os
+from google import genai
 
 def lambda_handler(event, context):
+    GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+    client = genai.Client(api_key=GEMINI_API_KEY)
+
+    response = client.models.generate_content(
+    model="gemini-2.5-flash-lite", contents="hello to me!"
+    )
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(response)
     }
