8949b50096bb22c0ffb7355ad1ea4595df320c7c
Hyeongjun Ham <hjham0856@gmail.com>
2025-10-30 20:20:39 +0900

    feat: 요청으로부터 url 및 body 추출, TODO 추가

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -10,6 +10,15 @@ def lambda_handler(event, context):
     model="gemini-2.5-flash-lite", contents="hello to me!"
     )
 
+    url = event['queryStringParameters']['url']
+    text_html = event['queryStringParameters']['body']
+
+    # TODO: 기존 URL 기반 캐싱 로직 구현
+
+    # TODO: text_html 문자열에서 중요 조항 위주로 약관 요약
+
+    # TODO: 각 요약된 조항에 대해 분석 수행
+
     return {
         'statusCode': 200,
         'body': json.dumps(response.text)
