65108f380bf88f66b9eda8d11daa5490df26ec16
Hyeongjun Ham <hjham0856@gmail.com>
2025-10-31 14:58:10 +0900

    feat: lambda_handler가 양식에 맞는 샘플 데이터를 반환하도록 수정 - 기능구현 전까지만 사용되고 제거 예정입니다.

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -21,5 +21,21 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps(response.text)
+        'body': json.dumps({
+            "overall_evaluation": "E",
+            "evaluation_for_each_clause": [
+                {
+                    "evaluation": "neutral",
+                    "summarized_clause": "이용자는 본 약관에 동의함으로써 당 서비스를 이용할 수 있습니다."
+                },
+                {
+                    "evaluation": "neutral",
+                    "summarized_clause": "당 회사는 이용자의 개인정보를 보호하기 위해 최선을 다합니다."
+                },
+                {
+                    "evaluation": "bad",
+                    "summarized_clause": "서비스 이용 중 발생하는 문제에 대해 당 회사는 책임을 지지 않습니다."
+                }
+            ]
+        }, ensure_ascii=False)
     }
